933d350c7bc4699887f3c5154183d83d789eb40b
ryougiitachi <ryougiitachi@gmail.com>
2018-02-01 23:27:15 +0800

    Make some changes.

--- test-maven-gallery/src/main/java/per/itachi/test/gallery/Entry.java
+++ test-maven-gallery/src/main/java/per/itachi/test/gallery/Entry.java
@@ -1,6 +1,8 @@
 package per.itachi.test.gallery;
 
 import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -11,6 +13,7 @@ import java.net.URL;
 import java.net.URLConnection;
 import java.util.List;
 import java.util.Map;
+import java.util.zip.GZIPInputStream;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,30 +35,38 @@ public class Entry {
 	private static final void test(String weblink) {
 		StringBuilder builder = new StringBuilder();
 		String strURL = weblink;
-		String strTmpFilePath = GalleryUtils.joinStrings(builder, "html/", strURL.substring(strURL.lastIndexOf("/") + 1), ".html");
+		String strTmpFileName = strURL.substring(strURL.lastIndexOf("/") + 1);
+		String strTmpFilePath1 = null;
+		String strResContentEncoding = null;
 		URL url = null;
 		URLConnection connection = null;
 		InputStream sis = null;
 		OutputStream sos = null;
 		byte[] buffer = new byte[8192];
 		int count = 0;
+		boolean isSuccessful = false;
 		try {
 			url = new URL(strURL);
 			connection = url.openConnection(Proxy.NO_PROXY);
 			fillURLHeaders(connection);
 			connection.connect();
+			strResContentEncoding = connection.getHeaderField("Content-Encoding");
+			strTmpFilePath1 = GalleryUtils.joinStrings(builder, "html/", strTmpFileName, ".", strResContentEncoding);
 			logURLHeaders(connection);
 			sis = connection.getInputStream();
-			sos = new BufferedOutputStream(new FileOutputStream(strTmpFilePath));
+			sos = new BufferedOutputStream(new FileOutputStream(strTmpFilePath1));
 			while ((count = sis.read(buffer)) > 0) {
 				sos.write(buffer, 0, count);
 			}
+			isSuccessful = true;
 		} 
 		catch (MalformedURLException e) {
 			logger.error(e.getMessage(), e);
+			isSuccessful = false;
 		}
 		catch (IOException e) {
 			logger.error(e.getMessage(), e);
+			isSuccessful = false;
 		}
 		finally {
 			if (sis != null) {
@@ -77,6 +88,53 @@ public class Entry {
 				}
 			}
 		}
+		if (!isSuccessful) {
+			return;
+		}
+		
+		String strTmpFilePath2 = null;
+		if (strTmpFileName.endsWith(".html")) {
+			strTmpFilePath2 = GalleryUtils.joinStrings(builder, "html/", strTmpFileName);
+		} 
+		else {
+			strTmpFilePath2 = GalleryUtils.joinStrings(builder, "html/", strTmpFileName, ".html");
+		}
+		InputStream is = null;
+		OutputStream os = null;
+		try {
+			is = new GZIPInputStream(new FileInputStream(strTmpFilePath1));
+			os = new BufferedOutputStream(new FileOutputStream(strTmpFilePath2));
+			while ((count = is.read(buffer)) > 0) {
+				os.write(buffer, 0, count);
+			}
+		} 
+		catch (FileNotFoundException e) {
+			logger.error(e.getMessage(), e);
+		} 
+		catch (IOException e) {
+			logger.error(e.getMessage(), e);
+		} 
+		finally {
+			if (is != null) {
+				try {
+					is.close();
+					is = null;
+				} 
+				catch (IOException e) {
+					logger.error(e.getMessage(), e);
+				}
+			}
+			if (os != null) {
+				try {
+					os.close();
+					os = null;
+				} 
+				catch (IOException e) {
+					logger.error(e.getMessage(), e);
+				}
+			}
+		}
+		//$("section.container div.content-wrap div.content article.article-content p>img")
 	}
 	
 	private static void fillURLHeaders(URLConnection connection) {
