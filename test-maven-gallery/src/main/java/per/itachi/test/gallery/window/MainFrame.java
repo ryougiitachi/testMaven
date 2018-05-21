649cb5263b341a5f2908f62596df24cf029c735b
ryougiitachi <ryougiitachi@gmail.com>
2018-05-21 17:58:55 +0800

    Added the function about window.

---
+++ test-maven-gallery/src/main/java/per/itachi/test/gallery/window/MainFrame.java
@@ -0,0 +1,26 @@
+package per.itachi.test.gallery.window;
+
+import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+
+public class MainFrame extends JFrame {
+	
+	private JMenuBar menuBar;
+	
+	private JMenuItem menuItemExit;
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1610123978299521378L;
+
+	public MainFrame() {
+		super();
+		initWindow();
+	}
+	
+	private void initWindow() {
+		
+	}
+}
