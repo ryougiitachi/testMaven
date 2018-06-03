460309e938f4629bf12a02e901a473570782ff41
ryougiitachi <ryougiitachi@gmail.com>
2018-06-03 22:25:43 +0800

    Added test-maven-struts2

---
+++ test-maven-struts2/src/main/java/per/itachi/test/struts2/model/DefaultAjaxInModel.java
@@ -0,0 +1,14 @@
+package per.itachi.test.struts2.model;
+
+public class DefaultAjaxInModel {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
---
+++ test-maven-struts2/src/main/java/per/itachi/test/struts2/model/DefaultAjaxOutModel.java
@@ -0,0 +1,14 @@
+package per.itachi.test.struts2.model;
+
+public class DefaultAjaxOutModel {
+	
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
