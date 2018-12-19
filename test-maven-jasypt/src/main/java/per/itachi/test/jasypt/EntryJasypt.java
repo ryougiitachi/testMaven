76ca8d3e1e60bdb99962a9bc78619cd690809c8f
ryougiitachi <ryougiitachi@gmail.com>
2018-12-19 17:19:10 +0800

    Added a new module called test-maven-jasypt, which is used to study jasypt.

---
+++ test-maven-jasypt/src/main/java/per/itachi/test/jasypt/EntryJasypt.java
@@ -0,0 +1,26 @@
+package per.itachi.test.jasypt;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.encryption.pbe.config.EnvironmentStringPBEConfig;
+
+public class EntryJasypt {
+
+	public static void main(String[] args) {
+		if (args.length <= 1) {
+			return;
+		}
+		EnvironmentStringPBEConfig config = new EnvironmentStringPBEConfig();
+		config.setAlgorithm("PBEWithMD5AndDES");
+		config.setPassword("homecredit-ip");
+		StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+		encryptor.setConfig(config);
+		String strDecryption = encryptor.decrypt(args[0]);
+		String strEncryption = encryptor.encrypt(args[1]);
+		System.out.println(strDecryption);
+		System.out.println(strEncryption);
+		int charN = '\n';
+		int charR = '\r';
+		System.out.printf("%04X %04X\n", charN, charR);
+	}
+
+}
