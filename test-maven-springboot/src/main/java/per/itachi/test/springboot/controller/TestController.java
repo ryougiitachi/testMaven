333382ba48bcc98edbd50c069ca61e4f3a2a7180
Ryougi Itachi <ryougiitachi@gmail.com>
2019-01-02 00:03:45 +0800

    springboot: Added some codes about testing max-thread and max-connection

--- test-maven-springboot/src/main/java/per/itachi/test/springboot/controller/TestController.java
+++ test-maven-springboot/src/main/java/per/itachi/test/springboot/controller/TestController.java
@@ -1,5 +1,7 @@
 package per.itachi.test.springboot.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -8,9 +10,18 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller("/api")
 public class TestController {
 	
+	private final Logger logger = LoggerFactory.getLogger(TestController.class);
+	
 	@RequestMapping(path="/status", method={RequestMethod.GET})
 	@ResponseBody
 	public String checkStatus() {
+		try {
+			logger.info("Start handling. ");
+			Thread.sleep(5000l);
+		} 
+		catch (InterruptedException e) {
+			logger.error("", e);
+		}
 		return "OK";
 	}
 }
