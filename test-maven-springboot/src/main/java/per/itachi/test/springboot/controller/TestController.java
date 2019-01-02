03c410f3780549db36356653b6ae91941ac5413a
Ryougi Itachi <ryougiitachi@gmail.com>
2019-01-02 22:57:29 +0800

    springboot: Correct defect about RequestMapping path.

--- test-maven-springboot/src/main/java/per/itachi/test/springboot/controller/TestController.java
+++ test-maven-springboot/src/main/java/per/itachi/test/springboot/controller/TestController.java
@@ -7,20 +7,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-@Controller("/api")
+@Controller("/api")//千万别当成RequestMapping；
+@RequestMapping("/api")
 public class TestController {
 	
 	private final Logger logger = LoggerFactory.getLogger(TestController.class);
 	
 	/**
-	 * RequestMapping的path如果是/开头可以无视Controller中的value？
+	 * RequestMapping的path如果是/开头可以无视Controller中的value？当然不是这样，之前把controller给当成RequestMapping；
 	 * */
 	@RequestMapping(path="/status", method={RequestMethod.GET})
 	@ResponseBody
 	public String checkStatus() {
 		try {
 			logger.info("Start handling. ");
-			Thread.sleep(5000l);
+			Thread.sleep(3000l);
 		} 
 		catch (InterruptedException e) {
 			logger.error("", e);
