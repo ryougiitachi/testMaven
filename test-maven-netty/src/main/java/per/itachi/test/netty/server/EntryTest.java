f4606c1adb432d993fcf1bcd655d6a8ced1168cc
ryougiitachi <ryougiitachi@gmail.com>
2019-01-30 17:57:18 +0800

    netty: Added TestServerChannelInitializer.

--- test-maven-netty/src/main/java/per/itachi/test/netty/server/EntryTest.java
+++ test-maven-netty/src/main/java/per/itachi/test/netty/server/EntryTest.java
@@ -5,11 +5,9 @@ import org.slf4j.LoggerFactory;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 public class EntryTest {
@@ -22,16 +20,12 @@ public class EntryTest {
 		ServerBootstrap server = new ServerBootstrap();
 		server.group(bossGrp, workerGrp);
 		server.channel(NioServerSocketChannel.class);
-		server.childHandler(new ChannelInitializer<SocketChannel>() {
-			@Override  
-			public void initChannel(SocketChannel ch) throws Exception {
-			}
-		});
+		server.childHandler(new TestServerChannelInitializer());
 		server.option(ChannelOption.SO_BACKLOG, 128);
 		server.childOption(ChannelOption.SO_KEEPALIVE, true);
 		ChannelFuture f = null;
 		try {
-			f = server.bind(10000).sync();
+			f = server.bind(10001).sync();
 		} 
 		catch (InterruptedException e) {
 			logger.error("", e);
