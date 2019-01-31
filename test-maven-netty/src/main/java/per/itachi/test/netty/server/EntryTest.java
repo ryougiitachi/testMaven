bf2eedda0e2bc915d44ea752b30311697e96399f
Ryougi Itachi <ryougiitachi@gmail.com>
2019-01-31 19:57:47 +0800

    netty: Added arguments function.

--- test-maven-netty/src/main/java/per/itachi/test/netty/server/EntryTest.java
+++ test-maven-netty/src/main/java/per/itachi/test/netty/server/EntryTest.java
@@ -1,15 +1,18 @@
 package per.itachi.test.netty.server;
 
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.SocketAddress;
+import java.net.UnknownHostException;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import io.netty.bootstrap.ServerBootstrap;
 import io.netty.channel.ChannelFuture;
-import io.netty.channel.ChannelInitializer;
 import io.netty.channel.ChannelOption;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
-import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
 
 public class EntryTest {
@@ -17,21 +20,40 @@ public class EntryTest {
 	private static final Logger logger = LoggerFactory.getLogger(EntryTest.class);
 
 	public static void main(String[] args) {
+		logger.info("{}", Runtime.getRuntime().availableProcessors());
+		
+		if (args.length <= 1) {
+			return;
+		}
+		
+		SocketAddress bindSocketAddress = null;
+		try {
+			String strIP = args[0];
+			String strPort = args[1];
+			int port = Integer.parseInt(strPort);
+			bindSocketAddress = getBindSocketAddress(strIP, port);
+			logger.info("{}", bindSocketAddress);
+		} 
+		catch (UnknownHostException e) {
+			logger.error(e.getMessage(), e);
+			return;
+		}
+		catch (RuntimeException e) {
+			logger.error(e.getMessage(), e);
+			return;
+		} 
+		
 		EventLoopGroup bossGrp = new NioEventLoopGroup();
 		EventLoopGroup workerGrp = new NioEventLoopGroup();
 		ServerBootstrap server = new ServerBootstrap();
 		server.group(bossGrp, workerGrp);
 		server.channel(NioServerSocketChannel.class);
-		server.childHandler(new ChannelInitializer<SocketChannel>() {
-			@Override  
-			public void initChannel(SocketChannel ch) throws Exception {
-			}
-		});
+		server.childHandler(new TestServerChannelInitializer());
 		server.option(ChannelOption.SO_BACKLOG, 128);
 		server.childOption(ChannelOption.SO_KEEPALIVE, true);
 		ChannelFuture f = null;
 		try {
-			f = server.bind(10000).sync();
+			f = server.bind(bindSocketAddress).sync();
 		} 
 		catch (InterruptedException e) {
 			logger.error("", e);
@@ -48,5 +70,11 @@ public class EntryTest {
 			workerGrp.shutdownGracefully();
 			bossGrp.shutdownGracefully();
 		}
+	}
+	
+	private static SocketAddress getBindSocketAddress(String ip, int port) throws UnknownHostException {
+		InetAddress ipAddress = InetAddress.getByName(ip);
+		SocketAddress socketAddress = new InetSocketAddress(ipAddress, port);
+		return socketAddress;
 	}
 }
