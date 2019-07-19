ec4facdd8eb8bba16360623a44ed463f282d282c
ryougiitachi <ryougiitachi@gmail.com>
2019-07-19 17:09:29 +0800

    springboot: Function about uploading file and tag libraries via springmvc.

---
+++ test-maven-springboot/src/main/java/per/itachi/test/springboot/controller/UploadController.java
@@ -0,0 +1,42 @@
+package per.itachi.test.springboot.controller;
+
+import java.io.IOException;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+@Controller
+@RequestMapping("/upload")
+public class UploadController {
+	
+	private final Logger logger = LoggerFactory.getLogger(UploadController.class);
+	
+	/**
+	 * 在需要返回页面的时候不要用RestController，这个注解会直接把返回值当做结果而不是ModelAndView；
+	 * */
+	@RequestMapping(path="/upload-index")
+	public String redirect() {
+		return "upload-index";
+	}
+	
+	@RequestMapping(path="/upload-file")
+	public String upload(@RequestParam MultipartFile fileUpload, HttpServletRequest request) throws IOException {
+		logger.info("fileUpload is {}", fileUpload);
+		logger.info("fileUpload.getBytes()'s length is {}", fileUpload.getBytes().length);
+		logger.info("fileUpload.getContentType() is {}", fileUpload.getContentType());
+		logger.info("fileUpload.getInputStream() is {}", fileUpload.getInputStream());
+		logger.info("fileUpload.getName() is {}", fileUpload.getName());
+		logger.info("fileUpload.getOriginalFilename() is {}", fileUpload.getOriginalFilename());
+		logger.info("fileUpload.getResource() is {}", fileUpload.getResource());
+		logger.info("fileUpload.getSize() is {}", fileUpload.getSize());
+		logger.info("fileUpload.isEmpty() is {}", fileUpload.isEmpty());
+		logger.info("request content-type is {}", request.getContentType());
+		return "success";
+	}
+}
---
+++ test-maven-springboot/src/main/webapp/template/index.jsp
@@ -0,0 +1,9 @@
+<html>
+	<body>
+		<h2>Hello World!</h2>
+		<form method="POST" enctype="multipart/form-data" action="/springboot/mvc/upload"> 
+			文件：<input type="file" name="fileUpload" />
+			<input type="submit" value="上传" />
+		</form>
+	</body>
+</html>
