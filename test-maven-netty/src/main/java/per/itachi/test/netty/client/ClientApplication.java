3b6bbc96cab57554f24fa5146cf6b489b529eb5c
Ryougi Itachi <ryougiitachi@gmail.com>
2020-04-10 18:13:04 +0800

    netty: Studied client side architecture using Netty.

---
+++ test-maven-netty/src/main/java/per/itachi/test/netty/client/ClientApplication.java
@@ -0,0 +1,12 @@
+package per.itachi.test.netty.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientApplication.class, args);
+	}
+}
